e2680e1e49810ed4ace47c44f8e2098dad45716b
yuriatgoogle <ygrinshteyn@google.com>
2017-08-14 10:52:54 -0700

    changing port in status message

--- http-node.js
+++ http-node.js
@@ -21,4 +21,4 @@ var server = http.createServer(function (request, response) {
 server.listen(8080);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running at http://127.0.0.1:8080/");
