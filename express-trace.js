68fa3c956cb180704322feab44c927dc47eae4b6
Yuri <ygrinshteyn@google.com>
2017-08-23 15:02:54 -0700

    changed to express, trace working now

---
+++ express-trace.js
@@ -0,0 +1,30 @@
+require('@google-cloud/trace-agent').start();
+
+//function for randomizing
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+const express = require('express');
+const app = express();
+
+
+
+// This incoming HTTP request should be captured by Trace
+app.get('/', (req, res) => {
+	var sleepInt = randomInt(1,10);
+	var sleepVar = require ('sleep');
+	sleepVar.sleep(sleepInt);
+	
+	res
+        .status(200)
+        .send("I slept for " + sleepInt + " seconds before responding")
+        .end();
+    });
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Server running at http://127.0.0.1:8080/");
+  console.log('Press Ctrl+C to quit.');
+});
--- http-node.js
+++ http-node.js
@@ -13,19 +13,28 @@ function randomInt (low, high) {
 }
 
 // Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, nodeResp) {
+var server = http.createServer(function (req, nodeResp) {
   var sleepInt = randomInt(1,10);
   var sleepVar = require ('sleep');
   sleepVar.sleep(sleepInt);
   
   //make call to Trace API
   got(DISCOVERY_URL, { json: true })
-  	.then((response) => {
+  	.then(response => {
+  		console.log("got a response");
+  		console.log("response was " + response);
+  		//console.log(response.requestUrl);
   		nodeResp.writeHead(200, {"Content-Type": "text/plain"});
   		nodeResp.end("I slept for " + sleepInt + " seconds");
-	});
+  		})
+  	.catch(error => {
+        console.log("an error happened");
+        //=> 'Internal server error ...' 
+    	});
 	// end trace
-});
+
+});//end server
+
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8080);
 
