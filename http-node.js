19f46f138f7d33f6b58ff0495642eb68a404b1ae
Yuri <ygrinshteyn@google.com>
2017-08-23 10:41:14 -0700

    turning sleep back on

--- http-node.js
+++ http-node.js
@@ -10,9 +10,9 @@ function randomInt (low, high) {
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   //console.log("Random number is " + randomInt(1,5));
-  var sleepInt = randomInt(1,5);
+  var sleepInt = randomInt(1,10);
   var sleepVar = require ('sleep');
-  //sleepVar.sleep(sleepInt);
+  sleepVar.sleep(sleepInt);
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("I slept for " + sleepInt + " seconds");
   //response.end("no sleep");
